baaead806941049721e2eb74d7795705d9e94f97
JoffreyNK <joyjoffrey@gmail.com>
2022-12-06 09:00:01 +0200

    add component folder

--- pages/index.js
+++
@@ -1,7 +0,0 @@
-function Homepage() {
-  return (
-    <h2>Welcome to Nextjs App</h2>
-  )
-}
-
-export default Homepage
--- pages/news.js
+++
@@ -1,7 +0,0 @@
-function news() {
-  return (
-    <h2>Get your News from Here</h2>
-  )
-}
-
-export default news
